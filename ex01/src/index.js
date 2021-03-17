74ed4ced4db8c3d0c7eecb1e3c23e5a078596b19
aanc1122 <amelanuhanovic997@gmail.com>
2021-03-17 21:34:40 +0100

    finished exercises

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+var myPet = {
+    species: "Poodle",
+    name: "Bella",
+    legs: 4,
+    friends: ["Coco", "Charlie"]
+};
+function myFunction(myObj) {
+    return myObj;
+}
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+var lion = {
+    "name": "Simba",
+    "legs": 4,
+    "tails": 1
+};
+function myFunction(objName, objValue) {
+    lion[objName] = objValue;
+    return lion;
+}
+myFunction("roar", "roar-roar");
+console.log(myFunction("roar", "roar-roar"));
+module.exports = myFunction;
